e8e65c0500e5224c00552658b1a769ee501c8966
Teddy <teddy@foxxing.com>
2020-07-04 20:11:00 +0900

    GameData 객체 추가

---
+++ Game/GameData.cpp
@@ -0,0 +1,16 @@
+#include "GameData.h"
+
+namespace GAME
+{
+
+GameData::GameData()
+{
+	initialize();
+}
+
+void GameData::initialize()
+{
+	// todo
+}
+
+}
---
+++ Game/GameData.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace GAME
+{
+
+class GameData
+{
+public:
+	GameData();
+	virtual ~GameData() {};
+
+private:
+
+	void initialize();
+};
+
+}
