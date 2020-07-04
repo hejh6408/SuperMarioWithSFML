87c0cdfd3ee49c2cf02ea98984ef2a1793298338
Teddy <teddy@foxxing.com>
2020-07-04 20:24:23 +0900

    Game 객체 추가

---
+++ Game/Game.cpp
@@ -0,0 +1,22 @@
+#include "Game.h"
+#include "GameData.h"
+
+namespace GAME
+{
+
+Game::Game(game_int _screenWidth, game_int _screenHeight, game_string _gameTitle)
+{
+	initialize();
+}
+
+GameState Game::Run()
+{
+	return GameState::FAIL;
+}
+
+void Game::initialize()
+{
+	// todo
+}
+
+}
