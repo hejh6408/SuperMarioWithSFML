4241ea56c18bea0b52e0936b4236afb31f925504
Teddy <teddy@foxxing.com>
2020-07-04 20:56:03 +0900

    main 반환 값 수정

--- main.cpp
+++ main.cpp
@@ -7,10 +7,9 @@ int main()
 {
 	GAME::Game marioGame(SCREEN_WIDTH, SCREEN_HEIGHT, GAME::game_string("Mario 2020"));
 
-	for(GAME::GameState state = marioGame.Run(); 
-		state == GAME::GameState::SUCCESS; state = marioGame.Run())
+	GAME::GameState state;
+	for(state = marioGame.Run(); state == GAME::GameState::SUCCESS; state = marioGame.Run())
 		;
 	
-
-	return EXIT_SUCCESS;
+	return state == GAME::GameState::SUCCESS ? EXIT_SUCCESS : EXIT_FAILURE;
 }
