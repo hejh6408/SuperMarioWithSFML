e5d1aa51107817c8e4f44080266073fe2daeced7
Teddy <teddy@foxxing.com>
2020-07-04 20:10:39 +0900

    Game 객체에 대한 정의 목록 추가

---
+++ Game/GameDefinition.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+namespace GAME
+{
+
+typedef int game_int;
+typedef std::string game_string;
+typedef bool game_bool;
+
+enum GameState
+{
+	FAIL,
+	SUCCESS
+};
+
+}
